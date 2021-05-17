fb619e229cff5ada50ba6e19b0a62b421bb770a4
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-05-17 08:23:27 +0530

    deleted templates

--- cowin_alert/views.py
+++ cowin_alert/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-import json 
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, response
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 import requests
@@ -13,25 +12,22 @@ import requests
 
 def py_api(request):
   headers = {'user-agent': 'Safari/14.1 (Macintosh; Intel macOS 11_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-  response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=294&date=16-05-2021", headers=headers)
+  response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=294&date=17-05-2021", headers=headers)
   data = response.json()
 
   for i in range(len(data['centers'])):
     if data['centers'][i]['sessions'][0]['min_age_limit'] == 18:
       names = data['centers'][i]['name']
       if data['centers'][i]['sessions'][0]['available_capacity'] > 0:
+        slots = data['centers'][i]['sessions'][0]['available_capacity']
         print(names)
+        print(slots)
         av_slots = data['centers'][i]['sessions'][0]['available_capacity'] 
         subject = 'Slots available!'
         message = f'{av_slots} available at {names}'
         email_from = settings.EMAIL_HOST_USER
         recipient_list = ['rshan.ali@gmail.com']
-        send_mail(subject, message, email_from, recipient_list
-            )
+        send_mail(subject, message, email_from, recipient_list)
 
 
-    
-      
-  return render(request, 'cowin_alert/py_api.html', {
-    'names': names
-  })
+  return HttpResponse('done')
