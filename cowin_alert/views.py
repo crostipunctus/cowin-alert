44976b1fd81360a57a4cbd7754b365f1c7283d25
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-05-17 10:37:02 +0530

    timer set

--- cowin_alert/views.py
+++ cowin_alert/views.py
@@ -1,33 +1,41 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-import requests
+import requests, threading, time
 
 
 # Create your views here.
 
-def py_api(request):
+def apicall():
   headers = {'user-agent': 'Safari/14.1 (Macintosh; Intel macOS 11_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
   response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=294&date=17-05-2021", headers=headers)
   data = response.json()
-
   for i in range(len(data['centers'])):
+    all_names = data['centers'][i]['name']
     if data['centers'][i]['sessions'][0]['min_age_limit'] == 18:
       names = data['centers'][i]['name']
-      if data['centers'][i]['sessions'][0]['available_capacity'] > 0:
+      
+      if data['centers'][i]['sessions'][0]['available_capacity_dose1'] > 0:
         slots = data['centers'][i]['sessions'][0]['available_capacity']
         print(names)
         print(slots)
-        av_slots = data['centers'][i]['sessions'][0]['available_capacity'] 
+        av_slots = data['centers'][i]['sessions'][0]['available_capacity_dose1'] 
         subject = 'Slots available!'
-        message = f'{av_slots} available at {names}'
+        message = f'{av_slots} available at {names}. Visit https://selfregistration.cowin.gov.in to book.'
         email_from = settings.EMAIL_HOST_USER
-        recipient_list = ['rshan.ali@gmail.com']
+        recipient_list = ['rshan.ali@gmail.com',]
         send_mail(subject, message, email_from, recipient_list)
+      else: 
+        print('no slots')
+
+  threading.Timer(5, apicall).start()
 
 
-  return HttpResponse('done')
+def py_api(request):
+  apicall()
+    
+  return HttpResponse('ok')
+
