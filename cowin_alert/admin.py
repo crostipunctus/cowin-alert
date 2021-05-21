0f28c4ef7d1aeb3fbd44f94e2b79d472d90e85f5
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-05-21 10:34:30 +0530

    added district id to user

--- cowin_alert/admin.py
+++ cowin_alert/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import District, Center
+from django.contrib.auth.models import User
+from .models import District, Center, User_details
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 
 # Register your models here.
+class UserDetailsInline(admin.StackedInline):
+    model = User_details
+    can_delete = False
+    verbose_name_plural = 'User details'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserDetailsInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 
 admin.site.register(Center)
 admin.site.register(District)
--- cowin_alert/models.py
+++ cowin_alert/models.py
@@ -4,15 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class District(models.Model):
+  district_name = models.CharField(max_length=100)
   district_id = models.IntegerField()
-  user_district = models.ForeignKey(User, on_delete=models.CASCADE)
-  
 
   def __str__(self):
-    return f'{self.user_district} => {self.district_id}'
+    return f'{self.district_name} => {self.district_id}'
+
+class User_details(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  user_district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+
 
 class Center(models.Model):
   center_district = models.ForeignKey(District, on_delete=models.CASCADE)
   center_name = models.CharField(max_length=100)
   center_slots = models.IntegerField()
 
+  def __str__(self):
+    return f'{self.center_name} has {self.center_slots} slots'
