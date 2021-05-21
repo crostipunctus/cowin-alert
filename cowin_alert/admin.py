dcc136720127cd710e554e0ca40cf8300fb54baa
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-05-21 10:04:23 +0530

    adding date to database

--- cowin_alert/admin.py
+++ cowin_alert/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import District
+from .models import District, Center
 
 # Register your models here.
 
-
+admin.site.register(Center)
 admin.site.register(District)
