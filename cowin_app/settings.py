389f18f0f90fb6002226c74582720eb34ba9c307
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-06-08 19:07:32 +0530

    requirements

--- cowin_app/settings.py
+++ cowin_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uae)5-n3$y8$ilptqys*4_&u*nj@%5o^#^%mp6o=zc*45qon4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
